1cbb0e4dc58158552b214ae16bfefafbaeb9cdc6
Steven Deutsch <stevensdeutsch@yahoo.com>
2016-04-01 18:25:34 -0500

    Add create recipe VC with imagepicker

---
+++ RecipezX/CreateRecipeViewController.swift
@@ -0,0 +1,61 @@
+//
+//  CreateRecipeViewController.swift
+//  RecipezX
+//
+//  Created by Steven on 4/1/16.
+//  Copyright © 2016 devslopes. All rights reserved.
+//
+
+import UIKit
+
+class CreateRecipeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var recipeTitle: UITextField!
+    @IBOutlet weak var recipeIngredients: UITextField!
+    @IBOutlet weak var recipeSteps: UITextField!
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var addRecipeButton: UIButton!
+    var imagePicker: UIImagePickerController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        recipeImage.layer.cornerRadius = 4.0
+        recipeImage.clipsToBounds = true
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        recipeImage.image = image
+    }
+    
+    @IBAction func addImage(sender: AnyObject!) {
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func createRecipe(sender: AnyObject!) {
+        if let title = recipeTitle.text where title != "" {
+            
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
